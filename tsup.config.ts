b646a576ba0ab6490573b4c57ac9048eebb60dfa
Casper Huang <casper.w.huang@qq.com>
2025-03-25 23:12:22 +0800

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: {
-    'index': 'src/index.ts',
-    'types/index': 'src/notation-types.ts',
-    'path-animator/index': 'src/path-animator.ts',
-    'path-generator/index': 'src/path-generator.ts',
-  },
+  entry: ['src/index.ts', 'src/notation-types.ts', 'src/path-animator.ts', 'src/path-generator.ts'],
+  // {
+  //   index: 'src/index.ts',
+  //   // 'types/index': 'src/notation-types.ts',
+  //   // 'path-animator/index': 'src/path-animator.ts',
+  //   // 'path-generator/index': 'src/path-generator.ts',
+  // },
   outDir: 'build',
   format: ['cjs', 'esm'],
   dts: true,
